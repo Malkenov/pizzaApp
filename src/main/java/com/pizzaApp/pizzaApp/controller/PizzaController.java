b61d2052c4e06ddcc440c442803879ae24ecd664
asanali_malkenov22@icloud.com <asanali_1997@mail.ru>
2025-11-12 23:33:34 +0500

    Второй коммит

---
+++ src/main/java/com/pizzaApp/pizzaApp/controller/PizzaController.java
@@ -0,0 +1,47 @@
+package com.pizzaApp.pizzaApp.controller;
+
+import com.pizzaApp.pizzaApp.dto.request.PizzaRequestDto;
+import com.pizzaApp.pizzaApp.dto.response.PizzaResponseDto;
+import com.pizzaApp.pizzaApp.service.PizzaService;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/pizza")
+public class PizzaController {
+
+    private PizzaService pizzaService;
+
+
+    @PostMapping
+    public ResponseEntity<PizzaResponseDto> postPizza(@RequestBody @Validated PizzaRequestDto dto){
+        return ResponseEntity.ok(pizzaService.postPizza(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PizzaResponseDto>> getAllPizza(){
+        return ResponseEntity.ok(pizzaService.getAllPizza());
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<PizzaResponseDto> getByName(@PathVariable String name){
+        return ResponseEntity.ok(pizzaService.getByName(name));
+    }
+
+    @PatchMapping("/{name}")
+    public ResponseEntity<PizzaResponseDto> updatePizza(@PathVariable String name, @RequestBody @Validated PizzaRequestDto dto){
+        return ResponseEntity.ok(pizzaService.updatePizza(name,dto));
+    }
+
+    @DeleteMapping("/{name}")
+    public ResponseEntity<Void> removePizza(@PathVariable String name){
+        pizzaService.deletePizza(name);
+        return ResponseEntity.noContent().build();
+    }
+}
