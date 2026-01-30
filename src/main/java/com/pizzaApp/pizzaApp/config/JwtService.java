eea925172c77e3703de0576d0c73bbff175e37fa
asanali_malkenov22@icloud.com <asanali_1997@mail.ru>
2026-01-30 20:26:19 +0500

    двадцатый третий коммит

--- src/main/java/com/pizzaApp/pizzaApp/config/JwtService.java
+++ src/main/java/com/pizzaApp/pizzaApp/config/JwtService.java
@@ -20,22 +20,36 @@ public class JwtService {
     public String extractUsername(String token){
         return extractClaim(token, Claims::getSubject);
     }
+    // Этот метод выдергивает email через токен. Метод вызывается при запросах на защищенный endpoint
+
+
+    /* --------------------------------------------------------------------------------*/
+
 
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
         final Claims claims = getAllClaimsFromToken(token);
         return claimsResolver.apply(claims);
     }
+    // Метод берет токен и с этого токена получает нужное поле с помощью "Function<Claims, T>" и возвращает ее
+
+
+    /* --------------------------------------------------------------------------------*/
 
     private Claims getAllClaimsFromToken(String token){
         return Jwts.parserBuilder()
-                .setSigningKey(getSignInKey())
+                .setSigningKey(getSignInKey()) // ключ, которым подписан токен
                 .build()
-                .parseClaimsJws(token)
-                .getBody();
+                .parseClaimsJws(token)         // парсит токен и проверяет подпись
+                .getBody();                    // возвращает Claims
     }
+    // Берет токен, проверяет его подпись, расшифровывает и возвращает все его поля
+
+
+    /* --------------------------------------------------------------------------------*/
 
     private SecretKey getSignInKey(){
         byte[] keyBytes = Decoders.BASE64.decode(jwtSecurityConfigProperties.getSecretKey());
         return Keys.hmacShaKeyFor(keyBytes);
     }
+    // Этот метод возвращает секретный ключ, который подписывает токен при создании и проверяет его
 }
